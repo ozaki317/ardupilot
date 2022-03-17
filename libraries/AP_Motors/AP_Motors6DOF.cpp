7359ea7246396ecb929d8f9c09a760c5acc52e55
kazu-06 <kazunari.masumoto@gmail.com>
2022-03-17 19:53:02 +0900

    masu

--- libraries/AP_Motors/AP_Motors6DOF.cpp
+++ libraries/AP_Motors/AP_Motors6DOF.cpp
@@ -183,7 +183,7 @@ void AP_Motors6DOF::setup_motors(motor_frame_class frame_class, motor_frame_type
         add_motor_raw_6dof(AP_MOTORS_MOT_3,     0,              0,               1.0f,          0.62f,              1.0f,               0,              3);
         add_motor_raw_6dof(AP_MOTORS_MOT_4,     0,              0,               -1.0f,         0.62f,              1.0f,               0,              4);
         add_motor_raw_6dof(AP_MOTORS_MOT_5,     0,              0,               1.0f,          -0.62f,             1.0f,               0,              5);
-        add_motor_raw_6dof(AP_MOTORS_MOT_6,     0,              0,               -1.0f,         -0.62f,             1.0f,               0,              6);
+        add_motor_raw_6dof(AP_MOTORS_MOT_6,     0,              0,               -1.0f,         -0.64f,             1.0f,               0,              6);
         break;
 
     case SUB_FRAME_SIMPLEROV_3:
